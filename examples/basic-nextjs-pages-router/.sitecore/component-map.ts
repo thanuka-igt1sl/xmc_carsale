dfcec346ebb093cd745a1c4589bb2091c43f65b8
Thanuka Pathirana <thanuka.pathirana@IGT1SL.COM>
2026-01-06 12:53:15 +0530

    First Commit

--- examples/basic-nextjs-pages-router/.sitecore/component-map.ts
+++ examples/basic-nextjs-pages-router/.sitecore/component-map.ts
@@ -1,18 +1,18 @@
 // Below are built-in components that are available in the app, it's recommended to keep them as is
+
 import { BYOCWrapper, NextjsContentSdkComponent, FEaaSWrapper } from '@sitecore-content-sdk/nextjs';
 import { Form } from '@sitecore-content-sdk/nextjs';
-// end of built-in components
 
-// Components imported from the app itself
+// end of built-in components
+import * as TitleAndText from 'src/components/TitleAndText';
 import * as PartialDesignDynamicPlaceholder from 'src/components/partial-design-dynamic-placeholder/PartialDesignDynamicPlaceholder';
 
-
-// Components must be registered within the map to match the string key with component name in Sitecore
 export const componentMap = new Map<string, NextjsContentSdkComponent>([
   ['BYOCWrapper', BYOCWrapper],
   ['FEaaSWrapper', FEaaSWrapper],
   ['Form', Form],
-  ['PartialDesignDynamicPlaceholder', PartialDesignDynamicPlaceholder],
+  ['TitleAndText', { ...TitleAndText }],
+  ['PartialDesignDynamicPlaceholder', { ...PartialDesignDynamicPlaceholder }],
 ]);
 
 export default componentMap;
