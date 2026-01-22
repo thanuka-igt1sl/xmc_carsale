99dfbc731f97698fe9f00e519ee94d0abe5bf0d5
Thanuka Pathirana <thanuka.pathirana@IGT1SL.COM>
2026-01-22 12:51:56 +0530

    Push C# News APP

---
+++ DemoBK/DemoNews/DemoNews/Entity/NewsItemEntity.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoNews.Entity
+{
+    public class NewsItemEntity
+    {
+        public Guid Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string Date { get; set; }
+
+        public string body { get; set; }
+        public string Url { get; set; }
+    }
+}
--- examples/basic-nextjs/src/components/demo/Button.tsx
+++ examples/basic-nextjs/src/components/demo/Button.tsx
@@ -1,5 +1,5 @@
-import React from 'react';
-import { Field, Text } from '@sitecore-content-sdk/nextjs';
+import React from "react";
+import { Field, Text } from "@sitecore-content-sdk/nextjs";
 interface Fields {
   name: Field<string>;
   style: Field<string>;
@@ -11,18 +11,25 @@ type ButtonProps = {
 };
 export const Default = (props: ButtonProps): React.ReactElement => {
   // 1. Handle Component Parameters (Standard Sitecore practice)
-  const containerStyles = props.params?.styles || '';
-  const gridStyles = props.params?.GridParameters || '';
+  const containerStyles = props.params?.styles || "";
+  const gridStyles = props.params?.GridParameters || "";
   const styles = `${gridStyles} ${containerStyles}`.trim();
 
   // 2. Fix: Access the .value property of the Field object
   // Also ensuring we handle cases where fields might be undefined
-  const buttonStyles = props.fields?.style?.value || ''; 
+  const buttonStyles = props.fields?.style?.value || "";
   return (
-    <div className={`container-default component ${styles}`}>      
-     <button type="button" className={buttonStyles}><Text field={props.fields.name} /></button>
-     <Text field={props.fields.icon} />    
+    <div className={`container-default component ${styles}`}>
+      <a href="#" className={buttonStyles} role="button">
+        {props.fields.icon?.value && (
+          <span
+            dangerouslySetInnerHTML={{ __html: props.fields.icon.value }}
+            className="w-5 h-5"
+          />
+        )}
+        <Text field={props.fields.name} />
+      </a>
+
     </div>
-    
   );
 };
