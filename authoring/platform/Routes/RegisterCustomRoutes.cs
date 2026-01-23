624cf4ca010ed8a9ab573f2369a224de3ed33c42
Thanuka Pathirana <thanuka.pathirana@IGT1SL.COM>
2026-01-23 12:48:39 +0530

    Push Test to the XMC

---
+++ authoring/platform/Routes/RegisterCustomRoutes.cs
@@ -0,0 +1,18 @@
+﻿using System.Web.Http;
+using System.Web.Routing;
+using Sitecore.Pipelines;
+
+namespace MyProject.Platform.Pipelines
+{
+    public class RegisterCustomRoutes
+    {
+        public void Process(PipelineArgs args)
+        {
+            GlobalConfiguration.Configure(config =>
+            {
+                // This enables the [Route] attributes used in Step 2
+                config.MapHttpAttributeRoutes();
+            });
+        }
+    }
+}
